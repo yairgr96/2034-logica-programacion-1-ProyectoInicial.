e9f66749cc9d24ca9492dd4351fd5a345889f2d7
yairgr96 <yairgr96@gmail.com>
2025-07-24 20:41:54 -0600

    Finaliza el juego de adivinar el número secreto con límite de intentos, mensajes personalizados y validación de respuestas.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //Variables
-let numeroSecreto = 7;
+let numeroMaximo = 10; //Número máximo del rango
+let numeroSecreto = Math.floor(Math.random() * numeroMaximo) + 1; //Genera un número aleatorio del 1 al 10
+//Nota: Math.floor() redondea hacia abajo y Math.random() genera un número decimal
 let numeroUsuario = 0;
-let intentos = 1;
-let palabra = 'vez';
+let intentos = 1; //Número de intentos permitidos
+//Palabra para pluralizar el mensaje final
+let palabra = 'Intento';
 
 //Bucle while. Mientras el número ingresado por el usuario sea diferente al número secreto, se repetirá el bucle.
 while (numeroUsuario !== numeroSecreto) {
 
     //Solicitar al usuario un número
-    numeroUsuario = prompt(`Introduce un número del 1 al 10`);
+    numeroUsuario = parseInt(prompt(`Introduce un número del 1 al ${numeroMaximo}`));
     //Acertar el número ingresado
     if (numeroUsuario == numeroSecreto) {
         alert(`¡Felicidades! Has adivinado el número secreto. ${numeroSecreto} en ${intentos} ${palabra}.`)
@@ -21,8 +24,9 @@ while (numeroUsuario !== numeroSecreto) {
         }
         //Contar los intentos
         intentos++;
-        palabra = 'veces';
-        if (intentos > 3) {
+        palabra = 'Intentos'; //Cambiar la palabra a plural
+        //Si el número de intentos supera el máximo permitido
+        if (intentos > 5) {
             alert(`Has superado el número máximo de intentos. El número secreto era ${numeroSecreto}.`);
             break;
         }
